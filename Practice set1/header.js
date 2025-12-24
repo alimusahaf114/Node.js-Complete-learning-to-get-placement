5d8e15717523a7d9bcb533b0a7e7712dfcbcfcac
Musahaf Ali <musahafali43@gmail.com>
2025-12-24 15:58:56 +0500

    added all

--- Practice set1/header.js
+++ Practice set1/header.js
@@ -2,20 +2,32 @@ const http = require('http')
 
 
 function showingHeader(req , res){
-console.log(req);
-res.setHeader("Content-Type", "text/html");
-    res.write('<html>');
+    if(req.url === "/home"){
+        res.write('<h1> Welcome to Home </h1>')
+    } else if(req.url === "/facebook") {
+        res.write('<h1> Welcome to Home </h1>')        
+    }else if(req.url === "/instagram") {
+        res.write('<h1> Welcome to Home </h1>')        
+    }else if(req.url === "/linkedin") {
+        res.write('<h1> Welcome to Home </h1>')        
+    }else if(req.url === "/twitter") {
+        res.write('<h1> Welcome to Home </h1>')        
+    } else {
+
+        res.setHeader("Content-Type", "text/html");
+        res.write('<html>');
     res.write('<head><title>Links</title></head>');
     res.write('<body>');
     res.write('<a href="/"> HOME </a><br>');
-    res.write('<a href="https://facebook.com"> FACEBOOK </a><br>');
-    res.write('<a href="https://instagram.com"> INSTAGRAM </a><br>');
-    res.write('<a href="https://linkedin.com"> LINKEDIN </a><br>');
-    res.write('<a href="https://whatsapp.com"> WHATSAPP </a><br>');
+    res.write('<a href="/facebook"> FACEBOOK </a><br>');
+    res.write('<a href="/instagram"> INSTAGRAM </a><br>');
+    res.write('<a href="/linkedin"> LINKEDIN </a><br>');
+    res.write('<a href="/twitter"> WHATSAPP </a><br>');
     res.write('</body>');
     res.write('</html>');
-
+    
     res.end(); // 🔥 VERY IMPORTANTres.end()
+}
 }
 
 const server = http.createServer(showingHeader)
