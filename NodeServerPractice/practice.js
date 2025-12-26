eea194fbc3a145380901861b005490d3e9e09c1f
Musahaf Ali <musahafali43@gmail.com>
2025-12-26 21:17:30 +0500

    added all code

--- NodeServerPractice/practice.js
+++ NodeServerPractice/practice.js
@@ -2,27 +2,54 @@ const http = require('http')
 
 
 function handleRequests(req , res){
-    res.setHeader('Content-Type' , 'text/html')
+    if(req.url == "/"){
+        res.setHeader('Content-Type' , 'text/html')
         res.write('<html>')
         res.write('<head><title>Welcome To Form</title></head>')
         res.write('<body>')
         res.write('<form action="submit-details" method="POST">')
-        res.write('<input type="text" name="username" placeholder="Enter your name"><br>')
-        res.write('<label for="male">Male</label>')
-        res.write('<input type="radio" id="male" name="gender" value="male" ><br>')
-        res.write('<label for="female">Female</label>')
-        res.write('<input type="radio" id="female" name="gender" value="female" ><br>')
-        res.write('<input type="submit"  value="submit" ><br>')
+        res.write('<h1></h1>')
+        res.write('</form>')
+        res.write('</body>')
+        res.write('</html>')
+    }else if(req.url == "/home"){
+        res.setHeader('Content-Type' , 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Welcome To Form</title></head>')
+        res.write('<body>')
+        res.write('<form action="submit-details" method="POST">')
+        res.write('<h1>Dear Musahaf this is Home</h1>')
         res.write('</form>')
         res.write('</body>')
         res.write('</html>')
         res.end()
+    }else if(req.url == "/blog"){
+        res.setHeader('Content-Type' , 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Welcome To Form</title></head>')
+        res.write('<body>')
+        res.write('<form action="submit-details" method="POST">')
+        res.write('<h1>Dear Musahaf this is Blog</h1>')
+        res.write('</form>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+}else {
+        res.setHeader('Content-Type' , 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Welcome To Form</title></head>')
+        res.write('<body>')
+        res.write('<form action="submit-details" method="POST">')
+        res.write('<h1 >Dear Musahaf this is Error</h1>')
+        res.write('</form>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+} 
 }
-
 
 const server = http.createServer(handleRequests)
 const PORT = 3000;
-
 server.listen(PORT , ()=>{
 console.log(`Server is Listening at http://localhost:${PORT}`);
 
